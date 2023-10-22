abfde094b95dffe116c05ba0ff82f67a9f384444
mservillon <mackservillon@gmail.com>
2023-10-21 19:24:08 -0500

    Router set up

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,31 @@ import React from 'react';
 
 import './App.css';
 
+import {
+  createHashRouter,
+  RouterProvider
+} from "react-router-dom";
+
 function App() {
+
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <div>Home</div>
+    },
+    {
+      path: "/setup",
+      element: <div>Setup</div>
+    },
+    {
+      path: "/play",
+      element: <div>Play</div>
+    },
+  ]);
   return (
-    <>
-    </>
+    <div className='App'>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
