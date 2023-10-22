bc041b98eb084e0701d7844d127a45f8ea4a8f18
mservillon <mackservillon@gmail.com>
2023-10-21 21:06:04 -0500

    Initial styled home screen layout and table

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { Home } from './Home';
 import { Play } from './Play';
 import { Setup } from './Setup';
 import { GameResult, getWinningPercentageDisplay } from './game-results';
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
+import { TableBarOutlined } from '@mui/icons-material';
 
 import {
   createHashRouter,
@@ -53,7 +55,48 @@ const App = () => {
   ]);
   return (
     <div className='App'>
-      <RouterProvider router={router} />
+      <Box
+        sx={{
+          flexGrow: 1
+        }}
+        >
+        <AppBar
+          position='static'
+          sx={{
+            overflow: 'hidden'
+            , bgcolor: 'gainsboro'
+          }}
+          >
+          <Toolbar>
+            <TableBarOutlined
+              color={"primary"}
+              sx={{
+                mr: 1
+                , fontSize: '1.5em'
+                , opacity: 0.5
+              }}
+              />
+              <Typography
+                variant='h6'
+                color='primary'
+                sx={{
+                  opacity: 0.75
+                }}
+              >
+                Klask Companion App
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <Box
+          sx={{
+            pl: 3
+            , pr: 3
+            , textAlign: 'center'
+          }}
+        >
+          <RouterProvider router={router} />
+        </Box>
     </div>
   );
 }
--- src/Home.tsx
+++ src/Home.tsx
@@ -2,7 +2,8 @@ import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 import { WinningPercentageDisplay } from './game-results';
 import { FC } from 'react';
-
+import { Typography, Paper, Table, TableBody, TableRow, TableCell } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 interface HomeProps {
     winningPercentageDisplay: WinningPercentageDisplay
 }
@@ -15,14 +16,93 @@ export const Home: FC<HomeProps> = ({winningPercentageDisplay}) => {
         <>
         <h3>Klask</h3>
         <Button
-            variant="outlined"
+            variant="contained"
             size="large"
+            sx={{
+                mt: 3
+                , mb: 3
+                , pt: 3
+                , pb: 3
+                , width: {
+                    xs: '100%'
+                    , md: 'inherit'
+                }
+            }}
             onClick={
                 () => navigate('setup')
             }
             >
+                <Typography
+                    fontSize={20}
+                >
                 Play Game
+                </Typography>
             </Button>
+            <Grid
+                container
+                spacing={3}
+            >
+                <Grid
+                    xs={12}
+                    md={6}
+                >
+                    <Paper
+                        elevation={3}
+                        sx={{
+                            overflow: 'hidden'
+                        }}
+                    >
+                        <Typography
+                            sx={{
+                                fontSize: 20
+                                , ml: 2
+                                , mt: 3
+                            }}
+                            color='text.disabled'
+                            gutterBottom
+                            >
+                                GENERAL
+                            </Typography>
+                            <Table>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell>
+                                            <Typography
+                                                fontSize={20}
+                                            >
+                                                Total Games
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography
+                                                fontSize={20}
+                                            >
+                                                {winningPercentageDisplay.totalGames}
+                                            </Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>
+                                            <Typography
+                                                fontSize={20}
+                                            >
+                                                Winning Percentage
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography
+                                                fontSize={20}
+                                            >
+                                                {winningPercentageDisplay.winningPercentage}
+                                            </Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </Paper>
+                </Grid>
+            </Grid>
+
             <h4>
                 {`Total: ${winningPercentageDisplay.totalGames}`}
             </h4>
