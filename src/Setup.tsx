3aa0082d9d518aeadc6e5052a67445f728fc9a76
mservillon <mackservillon@gmail.com>
2023-11-06 13:31:32 -0600

    Can now get the players that have been ch0sen

--- src/Setup.tsx
+++ src/Setup.tsx
@@ -17,6 +17,12 @@ export const Setup: FC<SetupProps> = ({
     , setTitle
     , previousPlayers
     }) => {
+
+        const [availablePlayers, setAvailablePlayers] = 
+        useState(previousPlayers.map(x => ({
+            name: x
+            , checked: true
+        })));
     
         setTitle("Game Setup")
     const navigate = useNavigate();
@@ -47,7 +53,7 @@ export const Setup: FC<SetupProps> = ({
                 }}
             >
                 {
-                    previousPlayers.map(x => (
+                    availablePlayers.map(x => (
                         <Grid
                             xs={12}
                             sm={6}
@@ -56,7 +62,22 @@ export const Setup: FC<SetupProps> = ({
                         >
                             <FormControlLabel 
                                 control={   
-                                    <Checkbox />} label={x} />
+                                    <Checkbox
+                                        checked={x.checked}
+                                        onChange={
+                                            (e) => setAvailablePlayers(
+                                                [
+                                                    ...availablePlayers.map(y => ({
+                                                        name: y.name
+                                                        , checked: y.name == x.name ? !y.checked : y.checked 
+                                                }))
+                                                ]
+                                            )
+                                        }
+                                    />
+                                    } 
+                                    label={x.name} 
+                                />
                         </Grid>
                     ))
                 }
